8cc69a72d2555936f6d73bbd5335d8381e44ba31
AliakseiBarysau <undergou@mail.ru>
2019-12-16 09:33:45 +0300

    third

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class'    => 'yii\db\Connection',
-    'dsn'      => 'mysql:host=basic;dbname=yii_auth',
-    'username' => 'root',
-    'password' => '',
-    'charset'  => 'utf8',
-];
--- config/params.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'adminEmail'  => 'lamer_10@mail.ru',
-    'senderEmail' => 'lamer_10@mail.ru',
-    'senderName'  => 'lamer_10@mail.ru',
-];
